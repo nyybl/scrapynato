eefe25184a4d7c0b84c1521197164dee968a388a
nyybl <nyybl@proton.me>
2025-01-12 08:38:42 +0530

    Add /api/v1/manga/{id}

--- routes/api/v1/manga.go
+++ routes/api/v1/manga.go
@@ -1,23 +1,33 @@
 package v1
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
-	"github.com/nyybl/scrapynato/lib"
 	"github.com/go-chi/chi/v5"
+	"github.com/nyybl/scrapynato/lib"
 )
 
 func MangaRouter() *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Get("/", lib.HandleHttp(GetIndex))
+	r.Get("/{id}", lib.HandleHttp(GetMangaByID))
 	return r
 }
 
-func GetIndex(w http.ResponseWriter, r *http.Request) lib.ResponseSchema {
-	return lib.NewResponse(http.StatusOK, "/")
-}
-
 func GetMangaByID(w http.ResponseWriter, r *http.Request) lib.ResponseSchema {
-	return lib.NewResponse(1, "")
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		return lib.NewErrorResponse(http.StatusBadRequest, errors.New("missing required parameter: id"))
+	}
+	d, err := lib.ScrapeManga(id)
+	if err != nil {
+		if err == lib.ErrNotFound {
+			return lib.NewErrorResponse(http.StatusNotFound, fmt.Errorf("could not manga with ID: ", id))
+		} else {
+			return lib.NewErrorResponse(http.StatusInternalServerError, err)
+		}
+	}
+	return lib.NewResponse(http.StatusOK, d)
 }
